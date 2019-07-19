b30ff0fb00e29e3fdaa2ee5f660cabde058aaca0
aliclint <aliclinton@gmail.com>
2019-07-19 10:08:22 -0400

    Up to date July 19

--- airplaneCrashAnalysisFunctions.R
+++ airplaneCrashAnalysisFunctions.R
@@ -1,4 +1,5 @@
-ExtractAirplaneUses <- function(x) {
+
+airplane_uses <- function(x) {
   if (grepl("Military",x,ignore.case=TRUE)) {
     return("Military")
   } else {
@@ -8,7 +9,7 @@ ExtractAirplaneUses <- function(x) {
 }
 
 
-ExtractCrashCauseKnown <- function(x) {
+airplane_known <- function(x) {
   if (grepl("unknown",x,ignore.case=TRUE)) {
     return("Known")
   } else {
@@ -20,7 +21,7 @@ ExtractCrashCauseKnown <- function(x) {
 library(maps) #library to string match with countries 
 ##https://stackoverflow.com/questions/47999506/matching-an-extracting-country-name-from-character-string-in-r
 
-ExtractCountry <- function(s) {
+extract_country <- function(s) {
   data(world.cities)
   raw <- s
   ###Removing punctuation
@@ -28,17 +29,8 @@ ExtractCountry <- function(s) {
   # Split data at word boundaries
   raw2 <- strsplit(raw, " ")
   # Match on country in world.countries
-  extracted.country <- (lapply(raw2, function(x)x[which(toupper(x) %in% toupper(world.cities$country.etc))]))
+  CountryList_raw <- (lapply(raw2, function(x)x[which(toupper(x) %in% toupper(world.cities$country.etc))]))
   
-  #extracted.country <- do.call(rbind, lapply(CountryList.Raw, as.data.frame))
-  extracted.country <- unlist(extracted.country)
-}
-
-ExtractLocation <- function(s) {
-  s <- sapply(s, as.character)
-  s <- gsub(".*,", "", s)
-  # remove white space trailing white space
-  s <- str_trim(s, side = "both")
-  s <- sapply(s, as.factor)
-  return(s)
+  extracted_country <- do.call(rbind, lapply(CountryList_raw, as.data.frame))
+  extracted_country <- unlist(extracted_country)
 }
